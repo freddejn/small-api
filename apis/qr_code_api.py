4c729949cf6f10d11601869e2084ccc23ee1f071
Fredrik Jonsson <freddejn@gmail.com>
2019-12-07 08:11:45 +0100

    Changed to return list QR-code.

--- apis/qr_code_api.py
+++ apis/qr_code_api.py
@@ -1,28 +1,18 @@
 import qrcode
 import qrcode.image.svg
 import io
-from flask_restplus import Namespace, Resource, reqparse, marshal
-from flask_marshmallow import Schema, fields
+from flask_restplus import Namespace, Resource, reqparse, marshal, fields
+from flask import make_response, jsonify
 
 from models.authorization import authorize_decorator
 
 api = Namespace('QR', description='QR-code api.')
 
 
-class QR_code(object):
-    def __init__(self, value, qr_code):
-        self.value = value
-        self.qr_code = qr_code
-
-    def __repr__(self):
-        return f'{self.value} value that goes into qr-code {self.qr_code} the qr-code svg'
-
-class 
-# qr_code_object =
-# qr_code_object = api.model(name='qr-code-object', model={
-#     'value': fields.String,
-#     'qr_code': fields.String,
-# })
+qr_code_object = api.model(name='qr-code-object', model={
+    'value': fields.String,
+    'qr_code': fields.String,
+})
 # qr_code_response = api.model(name='qr-code-array', model={
 #     'qr_codes': fields.List(fields.Nested(qr_code_object)),
 # })
@@ -32,8 +22,8 @@ class
 class QRApi(Resource):
     method_decorators = [authorize_decorator]
 
-    @api.marshal_with(qr_code_response)
-    @api.expect(qr_code_response)
+    @api.expect([qr_code_object], validate=False)
+    @api.response(200, 'Success', [qr_code_object])
     def post(self):
         factory = qrcode.image.svg.SvgImage
         QR_codes = []
@@ -48,5 +38,5 @@ class QRApi(Resource):
             byte_str = bytes_file.read()
             str_svg = byte_str.decode('utf-8')
             QR_codes.append({'value': value, 'qr_code': str_svg})
-        response = {'qr_codes': QR_codes}
-        return marshal(response, qr_code_response)
+        # response = {'qr_codes': QR_codes}
+        return QR_codes
